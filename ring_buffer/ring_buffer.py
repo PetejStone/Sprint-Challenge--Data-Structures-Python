3a9e89dd2ae8eebd59db6e2ab3656ff1ce9e67a5
PetejStone <petejstoney@gmail.com>
2019-09-20 08:47:23 -0700

    testing append and delete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,113 @@
+class ListNode:
+  def __init__(self, value, prev=None, next=None):
+    self.value = value
+    self.prev = prev
+    self.next = next
+
+# """Wrap the given value in a ListNode and insert it
+# after this node. Note that this node could already
+# have a next node it is point to."""
+  def insert_after(self, value):
+      current_next = self.next
+      self.next = ListNode(value, self, current_next)
+      if current_next:
+        current_next.prev = self.next
+
+# """Wrap the given value in a ListNode and insert it
+# before this node. Note that this node could already
+# have a previous node it is point to."""
+  def insert_before(self, value):
+      current_prev = self.prev
+      self.prev = ListNode(value, current_prev, self)
+      if current_prev:
+        current_prev.next = self.prev
+
+# """Rearranges this ListNode's previous and next pointers
+# accordingly, effectively deleting this ListNode."""
+  def delete(self):
+      if self.prev:
+        self.prev.next = self.next
+      if self.next:
+        self.next.prev = self.prev
+
+
 class RingBuffer:
-  def __init__(self, capacity):
+  def __init__(self, capacity, node=None):
     self.capacity = capacity
     self.current = 0
+    self.head = node
+    self.tail = node
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if len(self.storage) > 0: 
+      self.delete(self.tail)
+      # new_node = ListNode(item)
+      # print(new_node.value)
+      # new_node.prev = None
+      # self.head = new_node
+      # print(self.storage)
+    else:
+      pass
+      # new_node = ListNode(item)
+      # cur = self.head
+      # while cur.next:
+      #   pass
+  
+  def delete(self, key):
+      cur = self.head # set current item to the head
+      
+      while cur: #while cur is not Null, so while list is NOT empty
+        if cur == self.head: #if the current value is equal to the value we want to delete, and if it's currently at the head of the list
+        
+          ##Case 1 -- when the Node you want to delete is the head node, and it is also the only item
+          if not cur.next: #If not pointing to any node -- only node in list
+            cur = None # Removes current node we created for checking
+            self.head = None #removes node that matched, removes the head
+            
+            return cur## List is now empty, return
+          
+          #Case 2 - Delete Head node, but list is not empty (there is a next node)
+          else: #opposite of line 113, if next is pointing to a valid node
+            nxt = cur.next # set nxt == to the cur.next (the next item in the list (next to the head))
+            cur.next = None #We want to remove the pointer to the next item because it is being removed
+            #cur.prev = None  --- commented out because not needed, because head's previous already points to none
+            nxt.prev = None # setting this == to None because it's current prev was the deleted item
+            cur = None #get rid of element by setting it equal to None
+            self.head = nxt #move the status of the head to the nxt item
+            
+            return nxt#return the list
+
+          
+        else: #if data = key, but is not the head (elif from line 110)
+            #Case 3 -- When cur.next is NOT none, and NOT the head (if it has a prev and a next)
+            if cur.next: #if cur.next is true 
+              nxt = cur.next #set nxt to the next item
+              prev = cur.prev #set prev to the prev item -- see below
+              ##### ---- so in   A  B  C, if B is the cur, then cur.next (nxt) is C, and cur.prev (prev) is A
+              #### this is so we can get these to point to each other, looping around the 'deleted' item
+              prev.next = nxt #  so A.next is == C, skipping B
+              nxt.prev = prev # so C.prev == A skipping B
+              
+              #kill off pointers that aren't doing anything
+              cur.next = None
+              cur.prev = None
+              cur = None #Get rid of node
+              
+              return nxt # return list
+
+            #Case 4 if cur.next IS none, so if it is the tail
+            else: #if cur.next is none
+              prev = cur.prev # set the prev variable to the previous item
+              prev.next = None # Set to none because it will become the new tail
+            
+            #kill off pointers that aren't doing anything
+              cur.prev = None
+              cur = None # get rid of node
+              
+              return prev #return the list
+        cur = cur.next # moves to next pointer after each iteration through the list 
+      
 
   def get(self):
     pass
